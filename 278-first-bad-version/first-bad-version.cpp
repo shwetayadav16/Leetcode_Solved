16f67ae24b3aaab8ac439649d1d5a2fba6da901b
shwetayadav16 <124680771+shwetayadav16@users.noreply.github.com>
2025-02-10 22:25:01 +0530

    Time: 2 ms (54.61%) | Memory: 8 MB (3.66%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -6,17 +6,15 @@ public:
     int firstBadVersion(int n) {
        int low=1;
        int high=n;
-       int ans=-1;
        while(low<=high){
         int mid=low+(high-low)/2;
         if(isBadVersion(mid)){
-            ans=mid;
             high=mid-1;
         }
         else{
             low=mid+1;
         }
        } 
-       return ans;
+       return low;
     }
 };
