cd79757e896bf19576b4402f75c0eb71bff7e4f5
shwetayadav16 <124680771+shwetayadav16@users.noreply.github.com>
2025-02-13 10:46:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (79.01%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,0);
         if (n == 0) return 0; // Edge case: No house to rob
         if (n == 1) return nums[0]; // Edge case: Only one house
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
+        int prev1=nums[0];
+        int prev2=max(nums[0],nums[1]);
+        int curr=0;
         for(int i=2;i<n;i++){
-            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+            curr=max(nums[i]+prev1,prev2);
+            prev1=prev2;
+            prev2=curr;
         }
-        return dp[n-1];
+        return prev2;
     }
 };
