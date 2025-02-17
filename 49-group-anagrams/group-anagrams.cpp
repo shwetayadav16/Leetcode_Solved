face391ee0170a3c9aff0771c5dd274cbce8da06
shwetayadav16 <124680771+shwetayadav16@users.noreply.github.com>
2025-02-17 13:46:51 +0530

    Time: 1852 ms (5.05%) | Memory: 503.8 MB (5.18%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
+bool isValidAnagram(string &s, string &t) {
+        int n=s.size();
+        int m=t.size();
+        if(n!=m) return false;
+        vector<int>char_arr(26,0); 
+        for(int i=0;i<n;i++){
+            char_arr[s[i]-'a']++;
+        }
+        for(char ch:t){
+            char_arr[ch-'a']--;
+            if(char_arr[ch-'a']<0){
+                return false;
+            }
+        }
+        return true;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> anagramGroups; 
-
-        for (string& str : strs) {
-            string sortedStr = str;
-            sort(sortedStr.begin(), sortedStr.end()); 
-            
-            anagramGroups[sortedStr].push_back(str);
+        int n=strs.size();
+        vector<bool>visited(n,false);
+        vector<vector<string>>result;
+        for(int i=0;i<n;i++){
+            if(visited[i]) continue;
+            vector<string>group;
+            group.push_back(strs[i]);
+            visited[i]=true;
+            for(int j=i+1;j<n;j++){
+                if(isValidAnagram(strs[i],strs[j])&& !visited[j]){
+                    group.push_back(strs[j]);
+                    visited[j]=true;
+                }
+            }
+           
+            result.push_back(group);
         }
-
-        
-        vector<vector<string>> result;
-        for (auto& entry : anagramGroups) {
-            result.push_back(entry.second); 
-        }
-
         return result;
     }
 };
