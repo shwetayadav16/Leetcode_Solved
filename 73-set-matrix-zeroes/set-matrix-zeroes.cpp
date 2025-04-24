c230e9b54a1b033c812ccaf7dd3ef1a50fcdfc45
shwetayadav16 <124680771+shwetayadav16@users.noreply.github.com>
2025-04-24 16:05:05 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (79.40%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -5,20 +5,20 @@ public:
         int m=matrix[0].size();
         // int row[n] = {0}; --> matrix[..][0]
     // int col[m] = {0}; --> matrix[0][..]
-    int col0 = 1;
+    int row0 = 1;
     // step 1: Traverse the matrix and
     // mark 1st row & col accordingly:
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             if (matrix[i][j] == 0) {
                 // mark i-th row:
-                matrix[i][0] = 0;
+                matrix[0][j] = 0;
 
                 // mark j-th column:
-                if (j != 0)
-                    matrix[0][j] = 0;
+                if (i != 0)
+                    matrix[i][0] = 0;
                 else
-                    col0 = 0;
+                    row0 = 0;
             }
         }
     }
@@ -37,13 +37,13 @@ public:
 
     //step 3: Finally mark the 1st col & then 1st row:
     if (matrix[0][0] == 0) {
-        for (int j = 0; j < m; j++) {
-            matrix[0][j] = 0;
-        }
-    }
-    if (col0 == 0) {
         for (int i = 0; i < n; i++) {
             matrix[i][0] = 0;
+        }
+    }
+    if (row0 == 0) {
+        for (int j = 0; j < m; j++) {
+            matrix[0][j] = 0;
         }
     }
 
