b85101c749d5f8369f42a0303090419f65c562b9
Shweta Yadav <124680771+shwetayadav16@users.noreply.github.com>
2025-07-19 11:51:35 +0530

    Time: 2 ms (18.71%) | Memory: 9.4 MB (33.81%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-int all_poss(int i, int j,int m, int n,vector<vector<int>>&dp){
-//base cond
-if(i==m-1&&j==n-1) return 1;
-if(i>m-1|| j>n-1) return 0;
-if(dp[i][j]!=-1) return dp[i][j];
-int right=all_poss(i,j+1,m,n,dp);
-int down=all_poss(i+1,j,m,n,dp);
-return dp[i][j]=right+down;
-}
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1,vector<int>(n,-1));
-        return all_poss(0,0,m,n,dp);
+        vector<vector<int>> dp(m, vector<int>(n, 1)); // base case: 1 path in last row/col
+
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+            }
+        }
+
+        return dp[0][0];
     }
 };
