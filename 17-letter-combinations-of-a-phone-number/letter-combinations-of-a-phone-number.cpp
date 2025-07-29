439abb4aa409a47b8357731140afa44b6619f93d
Shweta Yadav <124680771+shwetayadav16@users.noreply.github.com>
2025-07-29 11:58:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (75.21%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    void combos(int ind, string digits, string str, vector<string>&ans,
-                unordered_map<char, string>& mp) {
-        if (ind == digits.size()) {
-            ans.push_back(str);
-            return;
-        }
-        char d=digits[ind];
-        string letters=mp[d];
-        for(char it:letters){
-            combos(ind+1,digits,str+it,ans,mp);
-        }
+
+void backtrack(int index,string digits,string &curr_string,unordered_map<char,string>&phonemap,vector<string>&result){
+    if(index==digits.size()){
+        result.push_back(curr_string);
+        return ;
     }
+    for(char c:phonemap[digits[index]]){
+        curr_string.push_back(c);
+        backtrack(index+1,digits,curr_string,phonemap,result);
+        curr_string.pop_back();
+    }
+}
     vector<string> letterCombinations(string digits) {
-        vector<string> ans;
-        if(digits.empty()) return ans;
-        unordered_map<char, string> mp = {{'2', "abc"}, {'3', "def"}, {'4', "ghi"},
-                                      {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"},
-                                      {'8', "tuv"}, {'9', "wxyz"}};
-        combos(0,digits,"", ans, mp);
-        return ans;
+        if(digits==""){
+            return {};
+        }
+
+        unordered_map<char,string>phonemap={
+                {'2',"abc"},{'3',"def"},{'4',"ghi"},{'5',"jkl"},{'6',"mno"},{'7',"pqrs"},{'8',"tuv"},{'9',"wxyz"}
+            };
+            vector<string>result;
+            string curr_string="";
+            backtrack(0,digits,curr_string,phonemap,result);
+            
+        return result;
     }
 };
