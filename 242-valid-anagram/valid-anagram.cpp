1a9ccf83e6e9eda385345f3800f7c33b0df8ba93
Shweta Yadav <124680771+shwetayadav16@users.noreply.github.com>
2025-08-06 10:55:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (26.14%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t) return true;
-        else return false;
+        if(s.size()!=t.size()) return false;
+        unordered_map<char,int>mp;
+        for(char ch:s){
+            mp[ch]++;
+        }
+        for(char ch:t){
+            if(mp.find(ch)==mp.end()||mp[ch]==0){
+                return false;
+            }
+            if(mp.find(ch)!=mp.end()){
+                mp[ch]--;
+            }
+        }
+        return true;
     }
 };
