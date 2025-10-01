50b7099c83f1e4cc56e109ff54011a250bf25819
Shweta Yadav <124680771+shwetayadav16@users.noreply.github.com>
2025-10-01 15:44:12 +0530

    Time: 2 ms (4.23%) | Memory: 9.8 MB (5.51%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int n = s.size(), length = 0;
-
-        for(int i=n-1;i>=0;i--){
-
-            if(length>0 && s[i] == ' ')break;
-
-            if(s[i] != ' ')length++;
-
+        stringstream ss(s);   // create string stream from s
+        string word;
+        while (ss >> word) {  // extract words separated by spaces
+            // word keeps updating, last word will remain
         }
-
-        return length;
+        return word.size();
     }
 };
