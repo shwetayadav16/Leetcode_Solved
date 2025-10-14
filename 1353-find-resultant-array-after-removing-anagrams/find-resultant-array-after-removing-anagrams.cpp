cabc421983fdfd2e593307b3a5b88450624e1f8c
Shweta Yadav <124680771+shwetayadav16@users.noreply.github.com>
2025-10-14 15:29:51 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (75.50%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-bool isAnagram(string word1,string word2){
-    sort(word1.begin(),word1.end());
-    sort(word2.begin(),word2.end());
-    return word1==word2;
-}
     vector<string> removeAnagrams(vector<string>& words) {
-        int n=words.size();
-        vector<string>res;
-        res.push_back(words[0]);
-        for(int i=1;i<n;i++){
-            if(isAnagram(words[i],res.back())){
-                continue;
-            }
-            res.push_back(words[i]);
-        }
-        return res;
+        vector<string> result;
+    string prevKey = "";
+
+    for (string word : words) {
+        string key = word;
+        sort(key.begin(), key.end());
+
+        if (key == prevKey) continue;
+        result.push_back(word);
+        prevKey = key;
+    }
+    return result;
     }
 };
