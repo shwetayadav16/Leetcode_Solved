9b7b639b3d166e96ed71b7b8125aad9e7d204039
Shweta Yadav <124680771+shwetayadav16@users.noreply.github.com>
2025-11-13 17:13:28 +0530

    Time: 10 ms (41.07%) | Memory: 14.4 MB (11.09%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,14 +9,14 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode*slow=head;
-        ListNode*fast=head;
-        while(fast!=NULL&&fast->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
-            if(slow==fast){
+        unordered_set<ListNode*>st;
+        ListNode*temp=head;
+        while(temp!=NULL){
+            if(st.find(temp)!=st.end()){
                 return true;
             }
+            st.insert(temp);
+            temp=temp->next;
         }
         return false;
     }
